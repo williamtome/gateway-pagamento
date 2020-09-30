19a7d43b713c8f51e9b92571f38eedd5501ae064
William Tomé <william.tomenh@gmail.com>
2020-09-30 11:06:46 -0300

    Build template show product.

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.welcome')
 
 @section('content')
-    {{ dd($product) }}
+
+    <div class="row">
+        <div class="col-6">
+            <p>Nome do produto: <strong>{{$product->name}}</strong></p>
+            <p>Marca: <strong>{{$product->brand}}</strong></p>
+            <h4>Valor: R$ <strong>{{$product->price}}</strong></h4>
+            <p>ou 10X R$ <strong>{{$product->price / 10}}</strong></p>
+            <form method="POST" action="{{route('carrinho.store')}}">
+                @csrf
+                <input type="hidden" name="item" value="{{$product->id}}">
+                <button class="btn btn-success btn-sm">Adicionar ao carrinho</button>
+            </form>
+        </div>
+    </div>
+
 @endsection

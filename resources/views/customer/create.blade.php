9115a47b9851f1eac0ac8ffadd3046b460ffd672
William Tomé <william.tomenh@gmail.com>
2020-10-03 16:53:07 -0300

    customer create view.

---
+++ resources/views/customer/create.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.welcome')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-12">
+            <form action="{{route('customer.store')}}" method="post">
+                <h1>Cadastro</h1>
+                <small>Por favor, insira os seus dados antes de efetuar o pagamento.</small>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="form-group">
+                            <label for="cpf">CPF</label>
+                            <input type="text" name="cpf" maxlength="11" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Nome completo</label>
+                            <input type="text" name="name" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">E-mail</label>
+                            <input type="email" name="email" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="phone">Telefone (Comercial ou celular)</label>
+                            <input type="text" name="phone" class="form-control" required>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <strong>Produto</strong>
+                        <p>{{$product->name}}</p>
+                        <strong>Marca</strong>
+                        <p>{{$product->brand}}</p>
+                        <strong>Valor</strong>
+                        <p>R$ {{$product->price}}</p>
+                        <button type="submit" class="btn btn-block btn-success">Próximo</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+@endsection

6d6533ec110b7749c2e55148c42672815179a127
William Tomé <william.tomenh@gmail.com>
2020-10-03 18:27:46 -0300

    class for validate data at requests on form customer.

---
+++ app/Http/Requests/CustomerRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\NomeCompleto;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CustomerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', new NomeCompleto()],
+            'cpf' => 'required|cpf',
+            'email' => 'required|email',
+            'phone' => 'required|digits_between:10,11'
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'O campo nome é obrigtório.',
+            'cpf.required' => 'O campo CPF é obrigtório.',
+            'cpf.cpf' => 'O CPF está inválido. Favor digite corretamente.',
+            'email.required' => 'O campo e-mail é obrigatório.',
+            'email.email' => 'O e-mail informado está incorreto.',
+            'phone.required' => 'O campo telefone é obrigatório.',
+            'phone.digits_between' => 'O telefone deve conter mínimo 10 e no máximo 11 caracteres.',
+        ];
+    }
+}

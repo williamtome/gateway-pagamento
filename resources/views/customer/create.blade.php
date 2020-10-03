96705669cc671fa05500fb8d3e4e60c7525a960a
William Tomé <william.tomenh@gmail.com>
2020-10-03 18:24:46 -0300

    change route and show messages error on each field.

--- resources/views/customer/create.blade.php
+++ resources/views/customer/create.blade.php
@@ -4,7 +4,8 @@
 
     <div class="row">
         <div class="col-12">
-            <form action="{{route('customer.store')}}" method="post">
+            <form action="{{route('cliente.store')}}" method="post">
+                @csrf
                 <h1>Cadastro</h1>
                 <small>Por favor, insira os seus dados antes de efetuar o pagamento.</small>
                 <div class="row">
@@ -12,18 +13,30 @@
                         <div class="form-group">
                             <label for="cpf">CPF</label>
                             <input type="text" name="cpf" maxlength="11" class="form-control" required>
+                            @error('cpf')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label for="name">Nome completo</label>
                             <input type="text" name="name" class="form-control" required>
+                            @error('name')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label for="email">E-mail</label>
                             <input type="email" name="email" class="form-control" required>
+                            @error('email')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label for="phone">Telefone (Comercial ou celular)</label>
                             <input type="text" name="phone" class="form-control" required>
+                            @error('phone')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
                     <div class="col-6">

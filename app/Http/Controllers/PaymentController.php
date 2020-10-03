eba2632053459a584791e28ce7b5c56dd566830c
William Tomé <william.tomenh@gmail.com>
2020-10-03 18:28:55 -0300

    payment controller created.

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Products;
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $product = Products::find(session()->get('carrinho.item'));
+        return view('payment.create', ['product' => $product]);
+    }
+}

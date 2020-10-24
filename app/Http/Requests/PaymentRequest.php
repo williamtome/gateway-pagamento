452b2a75180c805bda798982cf44962e283ac931
William Tomé <william.tomenh@gmail.com>
2020-10-24 19:49:11 -0300

    Classe para validar os campos do formulário de pagamento.

---
+++ app/Http/Requests/PaymentRequest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\NomeCompleto;
+use Illuminate\Foundation\Http\FormRequest;
+
+class PaymentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'card_number' => 'required|integer|max:16',
+            'card_name' => ['required', 'max:50', new NomeCompleto],
+            'card_validate' => 'required|date_format:mm/aaaa',
+            'card_cvv' => 'required|integer|max:3',
+            'installments' => 'required',
+            'cep' => 'required|max:8',
+            'address' => 'required|max:80',
+            'number' => 'required',
+            'complement' => 'required|max:40',
+            'district' => 'required|max:60',
+            'city' => 'required|min:2|max:60',
+            'state' => 'required',
+            'country' => 'required',
+        ];
+    }
+
+    /**
+     * Name of the attributes correspondent to fields.
+     *
+     * @return array
+     **/
+    public function attributes()
+    {
+        return [
+            'card_number' => 'Número do Cartão',
+            'card_name' => 'Nome do Dono do Cartão',
+            'card_validate' => 'Data de Validade do Cartão',
+            'card_cvv' => 'Código de segurança',
+            'installments' => 'Parcelas',
+            'cep' => 'CEP',
+            'address' => 'Endereço',
+            'complement' => 'Complemento',
+            'district' => 'Bairro',
+            'city' => 'Cidade',
+            'state' => 'Estado',
+            'country' => 'País',
+        ];
+    }
+}

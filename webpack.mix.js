328225e1ec9de23a54f4b9338b33a45657ea5402
William Tomé <william.tomenh@gmail.com>
2020-10-26 19:04:26 -0300

    adicionada formatação de telefone.

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,5 @@ const mix = require('laravel-mix');
 mix.styles('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css/bootstrap.min.css')
     .scripts('node_modules/jquery/dist/jquery.min.js', 'public/js/jquery.min.js')
     .scripts('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', 'public/js/bootstrap.min.js')
-    .scripts('node_modules/cleave.js/dist/cleave.min.js', 'public/js/cleave.min.js');
+    .scripts('node_modules/cleave.js/dist/cleave.min.js', 'public/js/cleave.min.js')
+    .scripts('node_modules/cleave.js/dist/addons/cleave-phone.br.js', 'public/js/cleave-phone.br.js');

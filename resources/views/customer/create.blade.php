2ee816fe1bde6142b25e5011c3f6c3a57644b727
William Tomé <william.tomenh@gmail.com>
2021-01-06 22:33:47 -0300

    Adicionado ao formulário, campos para preenchimento do endereço do cliente.

--- resources/views/customer/create.blade.php
+++ resources/views/customer/create.blade.php
@@ -38,6 +38,137 @@
                                 <div class="alert alert-danger">{{ $message }}</div>
                             @enderror
                         </div>
+                        <div class="mt-5">
+                            <h5>Endereço</h5>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label for="cep_customer">CEP</label>
+                                    <input value="{{ old('cep_customer') }}"
+                                           type="text"
+                                           name="cep_customer"
+                                           placeholder="00000-000"
+                                           class="cep_customer form-control @error('cep_customer') is-invalid @enderror"
+                                           required>
+                                    @error('cep_customer')
+                                    <div class="invalid-feedback">
+                                        {{$message}}
+                                    </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label for="street_customer">Rua *</label>
+                                    <input value="{{ old('street_customer') }}"
+                                           type="text"
+                                           name="street_customer"
+                                           class="form-control street_customer @error('street_customer') is-invalid @enderror"
+                                           required
+                                           readonly>
+                                    @error('street_customer')
+                                    <div class="invalid-feedback">
+                                        {{$message}}
+                                    </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label for="number_customer">Número*</label>
+                                    <input value="{{ old('number_customer') }}"
+                                           type="text"
+                                           name="number_customer"
+                                           maxlength="20"
+                                           class="form-control number_customer @error('number_customer') is-invalid @enderror"
+                                           required>
+                                    @error('number_customer')
+                                    <div class="invalid-feedback">
+                                        {{$message}}
+                                    </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label for="complement_customer">Complemento *</label>
+                                    <input value="{{ old('complement_customer') }}"
+                                           type="text"
+                                           id="complement_customer"
+                                           name="complement_customer"
+                                           class="form-control complement_customer @error('complement_customer') is-invalid @enderror"
+                                           required>
+                                    @error('complement_customer')
+                                    <div class="invalid-feedback">
+                                        {{$message}}
+                                    </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label for="district_customer">Bairro *</label>
+                                    <input value="{{ old('district_customer') }}"
+                                           type="text"
+                                           name="district_customer"
+                                           class="form-control district_customer @error('district_customer') is-invalid @enderror"
+                                           required
+                                           readonly>
+                                    @error('district_customer')
+                                    <div class="invalid-feedback">
+                                        {{$message}}
+                                    </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label for="city_customer">Cidade *</label>
+                                    <input value="{{ old('city_customer') }}"
+                                           type="text"
+                                           name="city_customer"
+                                           class="form-control city_customer @error('city_customer') is-invalid @enderror"
+                                           required
+                                           readonly>
+                                    @error('city_customer')
+                                    <div class="invalid-feedback">
+                                        {{$message}}
+                                    </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label for="state_customer">UF *</label>
+                                    <input value="{{ old('state_customer') }}"
+                                           type="text"
+                                           name="state_customer"
+                                           class="form-control state_customer @error('state_customer') is-invalid @enderror"
+                                           required
+                                           readonly>
+                                    @error('state_customer')
+                                    <div class="invalid-feedback">
+                                        {{$message}}
+                                    </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label for="country_customer">País *</label>
+                                    <select name="country_customer"
+                                            class="form-control @error('country_customer') is-invalid @enderror">
+                                        <option value="BRA" checked>Brasil</option>
+                                    </select>
+                                    @error('country_customer')
+                                    <div class="invalid-feedback">
+                                        {{$message}}
+                                    </div>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div class="col-6">
                         <strong>Produto</strong>
